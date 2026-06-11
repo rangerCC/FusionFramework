225b2114e150d5e912cdbeb12046124b9cdad8c5
Kevin <wugl0906@126.com>
2026-06-11 20:36:34 +0800

    返回接口回滚

--- SocialStory/SocialStoryCore/Theme/SSPageControllerBase.m
+++ SocialStory/SocialStoryCore/Theme/SSPageControllerBase.m
@@ -72,7 +72,7 @@
         [back setTitleColor:[SSTheme accentColor] forState:UIControlStateNormal];
         [back.titleLabel setFont:[UIFont systemFontOfSize:16]];
         back.accessibilityLabel = @"返回上一页";
-        [back addTarget:self action:@selector(onNavigationLeftButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+        [back addTarget:self action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
         [_naviBar setLeftView:back];
     }
 }
@@ -122,7 +122,7 @@
     return message;
 }
 
-- (void)onNavigationLeftButtonClick:(id)sender {
+- (void)goBack {
     if (![self canPopBack]) { return; }
     [[self getNavigator] poptoPage:[self backMessage]];
 }
